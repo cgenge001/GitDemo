e1cd5d220722c0855e5091fd0f534a1bfc2aec0e
cgenge001 <cgenge001@gmail.com>
2020-09-21 15:24:30 +0530

    added the code for string

--- palindrome.py
+++ palindrome.py
@@ -9,3 +9,8 @@ if(temp==rev):
     print("The number is palindrome!")
 else:
     print("Not a palindrome!")
+string=input(("Enter a string:"))
+if(string==string[::-1]):
+      print("The string is a palindrome")
+else:
+      print("Not a palindrome")
